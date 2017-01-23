736830c754a3586531baa57ca2025c14d3a74179
Ivan <madritistaaa@gmail.com>
2017-01-23 13:17:35 +0200

    Scaffold web app

---
+++ src/ZobShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZobShop.Web.Global" Language="C#" %>
---
+++ src/ZobShop.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZobShop.Web.Startup))]
+namespace ZobShop.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
