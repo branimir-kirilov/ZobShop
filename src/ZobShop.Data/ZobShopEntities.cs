516b6243c118370667cc29613eeca673a1de1a52
Ivan <madritistaaa@gmail.com>
2017-01-26 15:33:53 +0200

    Add db context

---
+++ src/ZobShop.Data/ZobShopEntities.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ZobShop.Models;
+
+namespace ZobShop.Data
+{
+    public class ZobShopEntities : IdentityDbContext<User>
+    {
+        public ZobShopEntities()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ZobShopEntities Create()
+        {
+            return new ZobShopEntities();
+        }
+    }
+}
