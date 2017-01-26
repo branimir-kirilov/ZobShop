aeb599eb2cfc10cbcd48ec4ec84ff87eacc89819
Ivan <madritistaaa@gmail.com>
2017-01-26 15:14:52 +0200

    Add user model

---
+++ src/ZobShop.Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ZobShop.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
