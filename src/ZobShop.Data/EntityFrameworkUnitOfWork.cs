70d1644d78908f7265d546c00ba307565769140f
Ivan <madritistaaa@gmail.com>
2017-01-26 15:26:57 +0200

    Implement unit of work

---
+++ src/ZobShop.Data/EntityFrameworkUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+using ZobShop.Data.Contracts;
+
+namespace ZobShop.Data
+{
+    public class EntityFrameworkUnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext dbContext;
+
+        public EntityFrameworkUnitOfWork(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("Context cannot be null");
+            }
+
+            this.dbContext = context;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+    }
+}
