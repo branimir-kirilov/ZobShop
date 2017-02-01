5dda3d7ccaead4dee757c92a33c3eaa33acc14dd
Ivan <madritistaaa@gmail.com>
2017-02-01 15:48:29 +0200

    Add ninject module for mvp

---
+++ src/ZobShop.Web/App_Start/NinjectModules/MvpNinjectModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Modules;
+using Ninject.Parameters;
+using WebFormsMvp;
+
+namespace ZobShop.Web.App_Start.NinjectModules
+{
+    public class MvpNinjectModule : NinjectModule
+    {
+        private const string ViewConstructorArgumentName = "view";
+
+        public override void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private IPresenter GetPresenter(IContext context)
+        {
+            var parameters = context.Parameters.ToList();
+
+            var presenterType = (Type)parameters[0].GetValue(context, null);
+            var view = (IView)parameters[1].GetValue(context, null);
+
+            var constructorParameter = new ConstructorArgument(ViewConstructorArgumentName, view);
+
+            var presenter = (IPresenter)context.Kernel.Get(presenterType, constructorParameter);
+            return presenter;
+        }
+    }
+}
