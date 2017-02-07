bbfa83ef787de5bdba83afd07f643dfb353d4324
Ivan <madritistaaa@gmail.com>
2017-02-07 20:02:14 +0200

    Add product list aspx

---
+++ src/ZobShop.Web/Product/ProductsList.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProductsList.aspx.cs" Inherits="ZobShop.Web.Product.ProductsList" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:DetailsView runat="server" ID="DetailsView" AutoGenerateRows="True"></asp:DetailsView>
+</asp:Content>
---
+++ src/ZobShop.Web/Product/ProductsList.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using WebFormsMvp;
+using WebFormsMvp.Web;
+using ZobShop.ModelViewPresenter.Product.List;
+
+namespace ZobShop.Web.Product
+{
+    [PresenterBinding(typeof(ProductListPresenter))]
+    public partial class ProductsList : MvpPage<ProductListViewModel>, IProductListView
+    {
+        public event EventHandler<ProductListEventArgs> MyInit;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var query = this.Request.QueryString;
+            var category = query?["category"];
+
+            var args = new ProductListEventArgs(category);
+            this.MyInit?.Invoke(this, args);
+
+            this.DetailsView.DataSource = this.Model.Products;
+            this.DataBind();
+        }
+    }
+}
