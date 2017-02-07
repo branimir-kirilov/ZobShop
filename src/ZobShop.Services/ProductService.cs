e262ac7a412a3b5dfcf23b7d44df6fa32cbea6f5
Ivan <madritistaaa@gmail.com>
2017-02-07 14:05:14 +0200

    Add validation

--- src/ZobShop.Services/ProductService.cs
+++ src/ZobShop.Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using ZobShop.Data.Contracts;
+﻿using System;
+using ZobShop.Data.Contracts;
 using ZobShop.Factories;
 using ZobShop.Models;
 using ZobShop.Services.Contracts;
@@ -17,6 +18,26 @@ namespace ZobShop.Services
             ICategoryService categoryService,
             IUnitOfWork unitOfWork)
         {
+            if (factory == null)
+            {
+                throw new ArgumentNullException("factory cannot be null");
+            }
+
+            if (productRepository == null)
+            {
+                throw new ArgumentNullException("repository cannot be null");
+            }
+
+            if (categoryService == null)
+            {
+                throw new ArgumentNullException("service cannot be null");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unit of work cannot be null");
+            }
+
             this.factory = factory;
             this.productRepository = productRepository;
             this.categoryService = categoryService;
