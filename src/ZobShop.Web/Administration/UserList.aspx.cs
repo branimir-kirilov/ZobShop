98d7385978e615137582c3d3f9b725ee7c028b1a
Ivan <madritistaaa@gmail.com>
2017-02-07 20:33:49 +0200

    Create user list page

---
+++ src/ZobShop.Web/Administration/UserList.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WebFormsMvp;
+using WebFormsMvp.Web;
+using ZobShop.ModelViewPresenter.Administration.UsersList;
+
+namespace ZobShop.Web.Administration
+{
+    [PresenterBinding(typeof(UserListPresenter))]
+    public partial class UserList : MvpPage<UserListViewModel>, IUserListView
+    {
+        public event EventHandler MyInit;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.MyInit?.Invoke(sender, e);
+        }
+    }
+}
