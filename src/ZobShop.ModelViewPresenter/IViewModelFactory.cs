79e150fa70337335c592f6daa9c04db1c5ad8678
Ivan <madritistaaa@gmail.com>
2017-02-07 16:20:24 +0200

    Add view model factory interface

---
+++ src/ZobShop.ModelViewPresenter/IViewModelFactory.cs
@@ -0,0 +1,9 @@
+﻿using ZobShop.ModelViewPresenter.Product.Details;
+
+namespace ZobShop.ModelViewPresenter
+{
+    public interface IViewModelFactory
+    {
+        ProductDetailsViewModel CreateProductDetailsViewModel(string name, string category, decimal price, double volume, string maker);
+    }
+}
