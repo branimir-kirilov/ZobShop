a55b28be2b16edeb3dbb0a5fda71ab519e602a74
Ivan <madritistaaa@gmail.com>
2017-02-07 13:08:13 +0200

    Create mvp for create product

---
+++ src/ZobShop.ModelViewPresenter/Product/Create/CreateProductEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ZobShop.ModelViewPresenter.Product.Create
+{
+    public class CreateProductEventArgs : EventArgs
+    {
+        public CreateProductEventArgs(string name, string categoryName, int quantity, decimal price, double volume, string maker)
+        {
+            this.Name = name;
+            this.CategoryName = categoryName;
+            this.Quantity = quantity;
+            this.Price = price;
+            this.Volume = volume;
+            this.Maker = maker;
+        }
+
+        public string CategoryName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public double Volume { get; set; }
+
+        public string Maker { get; set; }
+
+        public string Name { get; set; }
+    }
+}
