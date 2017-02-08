b2ef80d938db272f608eee5fb3aa197604c64c85
Ivan <madritistaaa@gmail.com>
2017-02-08 12:18:40 +0200

    Unit of work dispose tests

---
+++ src/ZobShop.Tests/Data/UnitOfWorkTests/DisposeTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Moq;
+using NUnit.Framework;
+using ZobShop.Data;
+
+namespace ZobShop.Tests.Data.UnitOfWorkTests
+{
+    [TestFixture]
+    public class DisposeTests
+    {
+        [Test]
+        public void TestDispose_ShouldNotThrow()
+        {
+            var dbContext = new Mock<DbContext>();
+
+            var unitOfWork = new UnitOfWork(dbContext.Object);
+
+            Assert.DoesNotThrow(() => unitOfWork.Dispose());
+        }
+    }
+}
