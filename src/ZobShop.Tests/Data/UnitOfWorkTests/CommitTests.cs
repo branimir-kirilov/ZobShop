b3d34c744f173ebf961d2660d9905447035e9aa3
Ivan <madritistaaa@gmail.com>
2017-02-08 12:14:12 +0200

    Unit of work tests

---
+++ src/ZobShop.Tests/Data/UnitOfWorkTests/CommitTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using Moq;
+using NUnit.Framework;
+using ZobShop.Data;
+
+namespace ZobShop.Tests.Data.UnitOfWorkTests
+{
+    [TestFixture]
+    public class CommitTests
+    {
+        [Test]
+        public void TestCommit_ShouldCallDbContextSaveChanges()
+        {
+            var dbContext = new Mock<DbContext>();
+
+            var unitOfWork = new UnitOfWork(dbContext.Object);
+
+            unitOfWork.Commit();
+
+            dbContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+    }
+}
---
+++ src/ZobShop.Tests/Data/UnitOfWorkTests/ConstructorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using Moq;
+using NUnit.Framework;
+using ZobShop.Data;
+
+namespace ZobShop.Tests.Data.UnitOfWorkTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassDbContextNull_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new UnitOfWork(null));
+        }
+
+        [Test]
+        public void TestConstructor_PassDbContext_ShouldNotThrow()
+        {
+            var dbContext = new Mock<DbContext>();
+
+            Assert.DoesNotThrow(() => new UnitOfWork(dbContext.Object));
+        }
+    }
+}
