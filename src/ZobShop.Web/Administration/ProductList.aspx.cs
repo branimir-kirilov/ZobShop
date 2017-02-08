97bce91af43d41d50c790e4eddd093f89cf6b93a
Ivan <madritistaaa@gmail.com>
2017-02-08 21:55:16 +0200

    Add product list aspx

---
+++ src/ZobShop.Web/Administration/ProductList.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebFormsMvp;
+using WebFormsMvp.Web;
+using ZobShop.ModelViewPresenter.Administration.ProductsList;
+using ZobShop.ModelViewPresenter.Product.List;
+
+namespace ZobShop.Web.Administration
+{
+    [PresenterBinding(typeof(ProductsListPresenter))]
+    public partial class ProductList : MvpPage<ProductListViewModel>, IProductsListView
+    {
+        public event EventHandler MyInit;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.MyInit?.Invoke(sender, e);
+
+            this.Products.DataSource = this.Model.Products;
+            this.DataBind();
+        }
+    }
+}
