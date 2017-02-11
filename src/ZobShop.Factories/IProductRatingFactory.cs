1ac54570bae1edb2ab3aff9abdba6930aa33e256
Ivan <madritistaaa@gmail.com>
2017-02-11 16:39:35 +0200

    Add product rating factory

---
+++ src/ZobShop.Factories/IProductRatingFactory.cs
@@ -0,0 +1,9 @@
+﻿using ZobShop.Models;
+
+namespace ZobShop.Factories
+{
+    public interface IProductRatingFactory
+    {
+        ProductRating CreateProductRating(int rating, string content, Product product, User author);
+    }
+}
