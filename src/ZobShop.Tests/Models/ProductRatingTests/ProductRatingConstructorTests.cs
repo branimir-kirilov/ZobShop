d5889723b1e71b297b7d3ec3578f240c797f333d
Ivan <madritistaaa@gmail.com>
2017-02-12 00:29:36 +0200

    Add product rating tests

---
+++ src/ZobShop.Tests/Models/ProductRatingTests/ProductRatingConstructorTests.cs
@@ -0,0 +1,78 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.Models;
+
+namespace ZobShop.Tests.Models.ProductRatingTests
+{
+    [TestFixture]
+    public class ProductRatingConstructorTests
+    {
+        [Test]
+        public void Constructor_ShouldBeInstanceOfProductRating()
+        {
+            var rating = new ProductRating();
+
+            Assert.IsInstanceOf<ProductRating>(rating);
+        }
+
+        [Test]
+        public void Constructor_Should_InitializeProductRatingCorrectly()
+        {
+            var productRating = new ProductRating();
+
+            Assert.IsNotNull(productRating);
+        }
+
+        [TestCase(1, "test")]
+        [TestCase(2, "tests")]
+        [TestCase(123, "test content")]
+        public void Constructor_ShouldSetRatingCorrectly(int rating, string content)
+        {
+            var mockedProduct = new Mock<Product>();
+            var mockedUser = new Mock<User>();
+
+            var productRating = new ProductRating(rating, content, mockedProduct.Object, mockedUser.Object);
+
+            Assert.AreEqual(productRating.Rating, rating);
+        }
+
+        [TestCase(1, "test")]
+        [TestCase(2, "tests")]
+        [TestCase(123, "test content")]
+        public void Constructor_ShouldSetContentCorrectly(int rating, string content)
+        {
+            var mockedProduct = new Mock<Product>();
+            var mockedUser = new Mock<User>();
+
+            var productRating = new ProductRating(rating, content, mockedProduct.Object, mockedUser.Object);
+
+            Assert.AreEqual(productRating.Content, content);
+        }
+
+        [TestCase(1, "test")]
+        [TestCase(2, "tests")]
+        [TestCase(123, "test content")]
+        public void Constructor_ShouldSetProductCorrectly(int rating, string content)
+        {
+            var mockedProduct = new Mock<Product>();
+            var mockedUser = new Mock<User>();
+
+            var productRating = new ProductRating(rating, content, mockedProduct.Object, mockedUser.Object);
+
+            Assert.AreEqual(productRating.Product, mockedProduct.Object);
+        }
+
+        [TestCase(1, "test")]
+        [TestCase(2, "tests")]
+        [TestCase(123, "test content")]
+        public void Constructor_ShouldSetAuthorContentCorrectly(int rating, string content)
+        {
+            var mockedProduct = new Mock<Product>();
+            var mockedUser = new Mock<User>();
+
+            var productRating = new ProductRating(rating, content, mockedProduct.Object, mockedUser.Object);
+
+            Assert.AreEqual(productRating.Author, mockedUser.Object);
+        }
+    }
+}
---
+++ src/ZobShop.Tests/Models/ProductRatingTests/PropertiesTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using ZobShop.Models;
+
+namespace ZobShop.Tests.Models.ProductRatingTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase("abcd")]
+        [TestCase("a2g2-17")]
+        public void TestAuthorId_ShouldWorkCorrectly(string authorId)
+        {
+            var productRating = new ProductRating();
+            productRating.AuthorId = authorId;
+
+            Assert.AreEqual(authorId, productRating.AuthorId);
+        }
+
+        [TestCase(1)]
+        [TestCase(12)]
+        [TestCase(1496)]
+        public void TestProductId_ShouldWorkCorrectly(int productId)
+        {
+            var productRating = new ProductRating();
+            productRating.ProductId = productId;
+
+            Assert.AreEqual(productId, productRating.ProductId);
+        }
+
+        [TestCase(1)]
+        [TestCase(12)]
+        [TestCase(1496)]
+        public void TestProductRatingId_ShouldWorkCorrectly(int productRatingId)
+        {
+            var productRating = new ProductRating();
+            productRating.ProductRatingId = productRatingId;
+
+            Assert.AreEqual(productRatingId, productRating.ProductRatingId);
+        }
+    }
+}
