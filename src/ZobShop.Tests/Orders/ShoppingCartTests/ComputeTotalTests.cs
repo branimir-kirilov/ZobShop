79ea4c819c6de60d0e1f20f9b952d77abdcf14b5
Ivan <madritistaaa@gmail.com>
2017-02-14 18:56:47 +0200

    Compute total test

---
+++ src/ZobShop.Tests/Orders/ShoppingCartTests/ComputeTotalTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.Orders;
+using ZobShop.Orders.Factories;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Tests.Orders.ShoppingCartTests
+{
+    [TestFixture]
+    public class ComputeTotalTests
+    {
+        [Test]
+        public void TestComputeTotal_EmptyCart_ShouldReturnZero()
+        {
+            var zero = 0m;
+
+            var mockedService = new Mock<IProductService>();
+            var mockedFactory = new Mock<ICartLineFactory>();
+
+            var cart = new ShoppingCart(mockedFactory.Object, mockedService.Object);
+
+            var result = cart.ComputeTotal();
+
+            Assert.AreEqual(zero, result);
+        }
+    }
+}
