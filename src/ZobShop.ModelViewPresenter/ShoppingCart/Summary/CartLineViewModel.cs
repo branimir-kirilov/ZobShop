7edd330803cca95820310b1ea83ad4929239234f
Ivan <madritistaaa@gmail.com>
2017-02-14 18:38:23 +0200

    Add view model

--- src/ZobShop.ModelViewPresenter/ShoppingCart/Summary/CartLineViewModel.cs
+++ src/ZobShop.ModelViewPresenter/ShoppingCart/Summary/CartLineViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace ZobShop.ModelViewPresenter.ShoppingCart.Summary
+﻿using ZobShop.ModelViewPresenter.Product.Details;
+
+namespace ZobShop.ModelViewPresenter.ShoppingCart.Summary
 {
     public class CartLineViewModel
     {
+        public CartLineViewModel(ProductDetailsViewModel model, int quantity)
+        {
+            this.Model = model;
+            this.Quantity = quantity;
+        }
+
+        public int Quantity { get; set; }
+
+        public ProductDetailsViewModel Model { get; set; }
     }
 }
--- src/ZobShop.ModelViewPresenter/ShoppingCart/Summary/CartSummaryPresenter.cs
+++ src/ZobShop.ModelViewPresenter/ShoppingCart/Summary/CartSummaryPresenter.cs
@@ -1,4 +1,5 @@
-﻿using WebFormsMvp;
+﻿using System.Linq;
+using WebFormsMvp;
 using ZobShop.Orders.Contracts;
 
 namespace ZobShop.ModelViewPresenter.ShoppingCart.Summary
@@ -6,11 +7,42 @@ namespace ZobShop.ModelViewPresenter.ShoppingCart.Summary
     public class CartSummaryPresenter : Presenter<ICartSummaryView>
     {
         private readonly IShoppingCart cart;
+        private readonly IViewModelFactory factory;
 
-        public CartSummaryPresenter(ICartSummaryView view, IShoppingCart cart)
+        public CartSummaryPresenter(ICartSummaryView view, IShoppingCart cart, IViewModelFactory factory)
             : base(view)
         {
             this.cart = cart;
+            this.factory = factory;
+
+            this.View.MyInit += View_MyInit;
+        }
+
+        private void View_MyInit(object sender, System.EventArgs e)
+        {
+            var products = this.cart.CartLines
+                .Select(this.GetCartLineViewModel);
+
+            this.View.Model.Products = products;
+        }
+
+        private CartLineViewModel GetCartLineViewModel(ICartLine line)
+        {
+            var quantity = line.Quantity;
+            var product = line.Product;
+
+            var productDetails = this.factory.CreateProductDetailsViewModel(product.ProductId,
+                product.Name,
+                product.Category.Name,
+                product.Price,
+                product.Volume,
+                product.Maker,
+                product.ImageMimeType,
+                product.ImageBuffer);
+
+            var result = this.factory.CreateCartLineViewModel(productDetails, quantity);
+
+            return result;
         }
     }
 }
