09d50ea27ecbda0bd9e60f0a5dbaf399ba00e03d
Ivan <madritistaaa@gmail.com>
2017-02-14 11:36:39 +0200

    Add event args for product rating

---
+++ src/ZobShop.ModelViewPresenter/Product/Details/RateProduct/RateProductEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ZobShop.ModelViewPresenter.Product.Details.RateProduct
+{
+    public class RateProductEventArgs : EventArgs
+    {
+        public RateProductEventArgs(int rating, string content, int productId, string authorId)
+        {
+            this.Rating = rating;
+            this.Content = content;
+            this.ProductId = productId;
+            this.AuthorId = authorId;
+        }
+
+        public int Rating { get; set; }
+
+        public string Content { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string AuthorId { get; set; }
+    }
+}
