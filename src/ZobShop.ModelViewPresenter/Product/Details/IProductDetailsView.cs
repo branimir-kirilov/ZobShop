d6ae8a5be49635ff443df85b5b480dbc69d1fe3a
Ivan <madritistaaa@gmail.com>
2017-02-14 11:36:59 +0200

    Add event for details view

--- src/ZobShop.ModelViewPresenter/Product/Details/IProductDetailsView.cs
+++ src/ZobShop.ModelViewPresenter/Product/Details/IProductDetailsView.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using WebFormsMvp;
+using ZobShop.ModelViewPresenter.Product.Details.RateProduct;
 
 namespace ZobShop.ModelViewPresenter.Product.Details
 {
     public interface IProductDetailsView : IView<ProductDetailsViewModel>
     {
         event EventHandler<ProductDetailsEventArgs> MyProductDetails;
+
+        event EventHandler<RateProductEventArgs> RateProduct;
     }
 }
