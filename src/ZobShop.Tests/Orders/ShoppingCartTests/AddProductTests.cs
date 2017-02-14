49b4fe4e783c68d8e1e373e625d28112bc6b4164
Ivan <madritistaaa@gmail.com>
2017-02-14 19:38:20 +0200

    Add more shopping cart tests

---
+++ src/ZobShop.Tests/Orders/ShoppingCartTests/AddProductTests.cs
@@ -0,0 +1,83 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.Models;
+using ZobShop.Orders;
+using ZobShop.Orders.Contracts;
+using ZobShop.Orders.Factories;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Tests.Orders.ShoppingCartTests
+{
+    [TestFixture]
+    public class AddProductTests
+    {
+        [TestCase(1, 2)]
+        [TestCase(12, 13)]
+        public void TestAddProduct_EmptyLine_ShouldCallServiceGetById(int id, int quantity)
+        {
+            var mockedService = new Mock<IProductService>();
+            var mockedFactory = new Mock<ICartLineFactory>();
+
+            var cart = new ShoppingCart(mockedFactory.Object, mockedService.Object);
+
+            cart.AddItem(id, quantity);
+
+            mockedService.Verify(x => x.GetById(id), Times.Once);
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(12, 13)]
+        public void TestAddProduct_ServiceGetByIdDoesNotReturnProduct_ShouldNotCallFactoryCreateProduct(int id, int quantity)
+        {
+            var mockedService = new Mock<IProductService>();
+            mockedService.Setup(x => x.GetById(It.IsAny<int>())).Returns((Product)null);
+
+            var mockedFactory = new Mock<ICartLineFactory>();
+
+            var cart = new ShoppingCart(mockedFactory.Object, mockedService.Object);
+
+            cart.AddItem(id, quantity);
+
+            mockedFactory.Verify(x => x.CreateCartLine(It.IsAny<Product>(), It.IsAny<int>()), Times.Never);
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(12, 13)]
+        public void TestAddProduct_ServiceGetByIdDoesReturnProduct_ShouldCallFactoryCreateProduct(int id, int quantity)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var mockedService = new Mock<IProductService>();
+            mockedService.Setup(x => x.GetById(It.IsAny<int>())).Returns(mockedProduct.Object);
+
+            var mockedFactory = new Mock<ICartLineFactory>();
+
+            var cart = new ShoppingCart(mockedFactory.Object, mockedService.Object);
+
+            cart.AddItem(id, quantity);
+
+            mockedFactory.Verify(x => x.CreateCartLine(mockedProduct.Object, quantity), Times.Once);
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(12, 13)]
+        public void TestAddProduct_ServiceGetByIdDoesReturnProduct_ShouldAddMockedLineToLines(int id, int quantity)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var mockedService = new Mock<IProductService>();
+            mockedService.Setup(x => x.GetById(It.IsAny<int>())).Returns(mockedProduct.Object);
+
+            var mockedLine = new Mock<ICartLine>();
+            var mockedFactory = new Mock<ICartLineFactory>();
+            mockedFactory.Setup(x => x.CreateCartLine(It.IsAny<Product>(), It.IsAny<int>()))
+                .Returns(mockedLine.Object);
+
+            var cart = new ShoppingCart(mockedFactory.Object, mockedService.Object);
+
+            cart.AddItem(id, quantity);
+
+            Assert.IsTrue(cart.CartLines.Contains(mockedLine.Object));
+        }
+    }
+}
---
+++ src/ZobShop.Tests/Orders/ShoppingCartTests/RemoveItemTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+using ZobShop.Orders;
+using ZobShop.Orders.Factories;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Tests.Orders.ShoppingCartTests
+{
+    [TestFixture]
+    public class RemoveItemTests
+    {
+        [TestCase(1)]
+        [TestCase(1278)]
+        [TestCase(15)]
+        public void TestRemoveItem_EmptyCollection_ShouldNotDoAnything(int productId)
+        {
+            var mockedService = new Mock<IProductService>();
+            var mockedFactory = new Mock<ICartLineFactory>();
+
+            var cart = new ShoppingCart(mockedFactory.Object, mockedService.Object);
+
+            cart.RemoveItem(productId);
+
+            Assert.IsFalse(cart.CartLines.Any());
+        }
+    }
+}
