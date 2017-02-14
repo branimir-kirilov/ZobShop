8a1182e2fd848fd78cfa90f4deefae5d547bc851
Ivan <madritistaaa@gmail.com>
2017-02-14 12:57:48 +0200

    Add edit page

---
+++ src/ZobShop.Web/Administration/Edit.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WebFormsMvp.Web;
+using ZobShop.ModelViewPresenter.Administration.EditProduct;
+using ZobShop.ModelViewPresenter.Product.Details;
+
+namespace ZobShop.Web.Administration
+{
+    public partial class Edit : MvpPage<ProductDetailsViewModel>, IEditProductView
+    {
+        public event EventHandler<ProductDetailsEventArgs> MyInit;
+
+        public event EventHandler<EditProductEventArgs> ProductEdit;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // TODO: Add UI
+
+            try
+            {
+                var id = int.Parse(this.Context.Request.QueryString["id"]);
+                var args = new ProductDetailsEventArgs(id);
+
+                this.MyInit?.Invoke(this, args);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
