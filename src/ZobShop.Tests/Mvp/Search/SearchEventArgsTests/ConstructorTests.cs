61811e231372c30fd41d2a2ee6539fba62847604
Ivan <madritistaaa@gmail.com>
2017-02-16 19:22:46 +0200

    Add search event args tests

---
+++ src/ZobShop.Tests/Mvp/Search/SearchEventArgsTests/ConstructorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ZobShop.ModelViewPresenter.Search;
+
+namespace ZobShop.Tests.Mvp.Search.SearchEventArgsTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase("str")]
+        [TestCase("search")]
+        [TestCase("lalala")]
+        [TestCase("query")]
+        public void TestConstructor_ShouldSetupSearchQueryStringCorrectly(string searchQuery)
+        {
+            var args = new SearchEventArgs(searchQuery);
+
+            Assert.AreEqual(searchQuery, args.SearchQueryString);
+        }
+
+
+        [TestCase("query")]
+        public void TestConstructor_ShouldInitializeCorrectly(string searchQuery)
+        {
+            var args = new SearchEventArgs(searchQuery);
+
+            Assert.IsNotNull(args);
+        }
+    }
+}
