c51ca6f0d2869b27f1f9b5f4ab39438a7be3b212
Ivan <madritistaaa@gmail.com>
2017-02-16 12:43:09 +0200

    Add search view

---
+++ src/ZobShop.Web/Search.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="ZobShop.Web.Search" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="col-md-12">
+        <h1><%: Title %>
+            <asp:Literal runat="server" ID="LiteralSearchQuery" Mode="Encode"></asp:Literal></h1>
+    </div>
+
+    <asp:Repeater runat="server" ID="Reapeater"
+         ItemType="ZobShop.ModelViewPresenter.Product.Details.ProductDetailsViewModel">
+        <HeaderTemplate>
+            <ul>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <li>
+                <a href="<%# string.Format("Product/ProductDetails?id={0}",Item.Id) %>"><%# $"{Item.Maker} {Item.Name}" %></a>
+
+                (Category: <%#: Item.Category %>)                 
+            </li>
+        </ItemTemplate>
+        <FooterTemplate>
+            </ul>
+        </FooterTemplate>
+    </asp:Repeater>
+
+    <div class="back-link">
+        <a href="/">Back to books</a>
+    </div>
+</asp:Content>
---
+++ src/ZobShop.Web/Search.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.ModelBinding;
+using WebFormsMvp;
+using WebFormsMvp.Web;
+using ZobShop.ModelViewPresenter.Product.Details;
+using ZobShop.ModelViewPresenter.Product.List;
+using ZobShop.ModelViewPresenter.Search;
+
+namespace ZobShop.Web
+{
+    [PresenterBinding(typeof(SearchPresenter))]
+    public partial class Search : MvpPage<ProductListViewModel>, ISearchView
+    {
+        private const string SearchQueryParam = "search";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var searchQuery = this.Request.QueryString[SearchQueryParam];
+
+                var args = new SearchEventArgs(searchQuery);
+
+                this.MySearch?.Invoke(this, args);
+
+                this.Reapeater.DataSource = this.Model.Products;
+                this.DataBind();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public event EventHandler<SearchEventArgs> MySearch;
+
+        public IEnumerable<ProductDetailsViewModel> Reapeater_GetData([QueryString("q")] string query)
+        {
+            return this.Model.Products;
+        }
+    }
+}
