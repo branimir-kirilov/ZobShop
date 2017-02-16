9c5fada32cdb9442b89bd843f6ba9a83079f9a7c
Ivan <madritistaaa@gmail.com>
2017-02-16 19:36:02 +0200

    Start search presenter view_mysearch tests

---
+++ src/ZobShop.Tests/Mvp/Search/SearchPresenterTests/ViewMySearchTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.ModelViewPresenter;
+using ZobShop.ModelViewPresenter.Product.List;
+using ZobShop.ModelViewPresenter.Search;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Tests.Mvp.Search.SearchPresenterTests
+{
+    [TestFixture]
+    public class ViewMySearchTests
+    {
+        [TestCase("search")]
+        [TestCase("much")]
+        public void TestViewMySearch_ShouldCallServiceSearchProductsCorrectly(string searchQuery)
+        {
+            var mockedModel = new Mock<ProductListViewModel>();
+
+            var mockedView = new Mock<ISearchView>();
+            mockedView.Setup(x => x.Model).Returns(mockedModel.Object);
+
+            var mockedFactory = new Mock<IViewModelFactory>();
+            var mockedService = new Mock<IProductService>();
+
+            var presenter = new SearchPresenter(mockedView.Object, mockedService.Object, mockedFactory.Object);
+
+            var args = new SearchEventArgs(searchQuery);
+            mockedView.Raise(x => x.MySearch += null, args);
+
+            mockedService.Verify(x => x.SearchProducts(searchQuery), Times.Once);
+        }
+    }
+}
