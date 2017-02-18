0bf9acf2152258e6977174152256d1a83d7a6166
Ivan <madritistaaa@gmail.com>
2017-02-18 19:15:07 +0200

    Add more login presenter constructor tests

--- src/ZobShop.Mvp.Tests/Account/LoginPresenterTests/ConstructorTests.cs
+++ src/ZobShop.Mvp.Tests/Account/LoginPresenterTests/ConstructorTests.cs
@@ -1,5 +1,7 @@
-﻿using Moq;
+﻿using System;
+using Moq;
 using NUnit.Framework;
+using ZobShop.Authentication;
 using ZobShop.ModelViewPresenter.Account.Login;
 
 namespace ZobShop.Mvp.Tests.Account.LoginPresenterTests
@@ -8,11 +10,30 @@ namespace ZobShop.Mvp.Tests.Account.LoginPresenterTests
     public class ConstructorTests
     {
         [Test]
-        public void TestConstructor_ShouldInitializeCorrectly()
+        public void TestConstructor_PassProviderNull_ShouldThrowArgumentNullException()
         {
             var mockedView = new Mock<ILoginView>();
 
-            var presenter = new LoginPresenter(mockedView.Object);
+            Assert.Throws<ArgumentNullException>(() => new LoginPresenter(mockedView.Object, null));
+        }
+
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldNotThrow()
+        {
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedView = new Mock<ILoginView>();
+
+            Assert.DoesNotThrow(() => new LoginPresenter(mockedView.Object, mockedProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_ShouldInitializeCorrectly()
+        {
+            var mockedView = new Mock<ILoginView>();
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            var presenter = new LoginPresenter(mockedView.Object, mockedProvider.Object);
 
             Assert.IsNotNull(presenter);
         }
