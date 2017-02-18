c6a6f32c24710812d5ae5a1f63d63915c62545c6
Ivan <madritistaaa@gmail.com>
2017-02-18 16:44:24 +0200

    Extract to new project

---
+++ src/ZobShop.Mvp.Tests/ShoppingCart/Add/AddPresenterTests/ConstructorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using ZobShop.ModelViewPresenter.ShoppingCart.Add;
+using ZobShop.Orders.Contracts;
+
+namespace ZobShop.Mvp.Tests.ShoppingCart.Add.AddPresenterTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassIShoppingCartNull_ShouldThrowArgumentNullException()
+        {
+            var mockedView = new Mock<IAddToCartView>();
+
+            Assert.Throws<ArgumentNullException>(() => new AddToCartPresenter(mockedView.Object, null));
+        }
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldNotThrow()
+        {
+            var mockedView = new Mock<IAddToCartView>();
+            var mockedCart = new Mock<IShoppingCart>();
+
+            Assert.DoesNotThrow(() => new AddToCartPresenter(mockedView.Object, mockedCart.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+        {
+            var mockedView = new Mock<IAddToCartView>();
+            var mockedCart = new Mock<IShoppingCart>();
+
+            var presenter = new AddToCartPresenter(mockedView.Object, mockedCart.Object);
+
+            Assert.IsNotNull(presenter);
+        }
+    }
+}
--- src/ZobShop.Tests/ModelViewPresenter/ShoppingCart/Add/AddPresenterTests/ConstructorTests.cs
+++ src/ZobShop.Tests/ModelViewPresenter/ShoppingCart/Add/AddPresenterTests/ConstructorTests.cs
@@ -1,40 +1,39 @@
 ﻿using System;
 using Moq;
 using NUnit.Framework;
-using ZobShop.ModelViewPresenter.ShoppingCart.Add;
 using ZobShop.Orders.Contracts;
 
 namespace ZobShop.Tests.ModelViewPresenter.ShoppingCart.Add.AddPresenterTests
 {
-    [TestFixture]
-    public class ConstructorTests
-    {
-        [Test]
-        public void TestConstructor_PassIShoppingCartNull_ShouldThrowArgumentNullException()
-        {
-            var mockedView = new Mock<IAddToCartView>();
+    //[TestFixture]
+    //public class ConstructorTests
+    //{
+    //    [Test]
+    //    public void TestConstructor_PassIShoppingCartNull_ShouldThrowArgumentNullException()
+    //    {
+    //        var mockedView = new Mock<IAddToCartView>();
 
-            Assert.Throws<ArgumentNullException>(() => new AddToCartPresenter(mockedView.Object, null));
-        }
+    //        Assert.Throws<ArgumentNullException>(() => new AddToCartPresenter(mockedView.Object, null));
+    //    }
 
-        [Test]
-        public void TestConstructor_PassEverythingCorrectly_ShouldNotThrow()
-        {
-            var mockedView = new Mock<IAddToCartView>();
-            var mockedCart = new Mock<IShoppingCart>();
+    //    [Test]
+    //    public void TestConstructor_PassEverythingCorrectly_ShouldNotThrow()
+    //    {
+    //        var mockedView = new Mock<IAddToCartView>();
+    //        var mockedCart = new Mock<IShoppingCart>();
 
-            Assert.DoesNotThrow(() => new AddToCartPresenter(mockedView.Object, mockedCart.Object));
-        }
+    //        Assert.DoesNotThrow(() => new AddToCartPresenter(mockedView.Object, mockedCart.Object));
+    //    }
 
-        [Test]
-        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
-        {
-            var mockedView = new Mock<IAddToCartView>();
-            var mockedCart = new Mock<IShoppingCart>();
+    //    [Test]
+    //    public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+    //    {
+    //        var mockedView = new Mock<IAddToCartView>();
+    //        var mockedCart = new Mock<IShoppingCart>();
 
-            var presenter = new AddToCartPresenter(mockedView.Object, mockedCart.Object);
+    //        var presenter = new AddToCartPresenter(mockedView.Object, mockedCart.Object);
 
-            Assert.IsNotNull(presenter);
-        }
-    }
+    //        Assert.IsNotNull(presenter);
+    //    }
+    //}
 }
