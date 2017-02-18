e95c13bfdde7ad57e83be9fc002e15fbc0445e86
Ivan <madritistaaa@gmail.com>
2017-02-18 16:51:12 +0200

    Finish add to cart presenter tests

---
+++ src/ZobShop.Mvp.Tests/ShoppingCart/Add/AddPresenterTests/ViewMyAddToCartTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.ModelViewPresenter.ShoppingCart.Add;
+using ZobShop.Orders.Contracts;
+
+namespace ZobShop.Mvp.Tests.ShoppingCart.Add.AddPresenterTests
+{
+    [TestFixture]
+    public class ViewMyAddToCartTests
+    {
+        [TestCase(1, 2)]
+        [TestCase(7, 4)]
+        [TestCase(13, 21)]
+        public void TestViewMyAddToCart_ShouldCallCartItemAddToCartCorrectly(int productId, int quantity)
+        {
+            var mockedView = new Mock<IAddToCartView>();
+            var mockedCart = new Mock<IShoppingCart>();
+
+            var presenter = new AddToCartPresenter(mockedView.Object, mockedCart.Object);
+
+            var args = new AddToCartEventArgs(productId, quantity);
+
+            mockedView.Raise(v => v.MyAddToCart += null, args);
+
+            mockedCart.Verify(c => c.AddItem(productId, quantity), Times.Once);
+        }
+    }
+}
