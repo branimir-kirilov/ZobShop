a4c834b52d04a3ffc7181c4d4d5d515c3d9721f6
Ivan <madritistaaa@gmail.com>
2017-02-18 18:06:37 +0200

    Search presenter constructor tests

---
+++ src/ZobShop.Mvp.Tests/SearchPresenterTests/ConstructorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using ZobShop.ModelViewPresenter;
+using ZobShop.ModelViewPresenter.Search;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Mvp.Tests.SearchPresenterTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassServiceNull_ShouldThrowArgumentNullException()
+        {
+            var mockedView = new Mock<ISearchView>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            Assert.Throws<ArgumentNullException>(() => new SearchPresenter(mockedView.Object, null, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassFactoryNull_ShouldThrowArgumentNullException()
+        {
+            var mockedView = new Mock<ISearchView>();
+            var mockedService = new Mock<IProductService>();
+
+            Assert.Throws<ArgumentNullException>(() => new SearchPresenter(mockedView.Object, mockedService.Object, null));
+        }
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldNotThrow()
+        {
+            var mockedView = new Mock<ISearchView>();
+            var mockedService = new Mock<IProductService>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            Assert.DoesNotThrow(() => new SearchPresenter(mockedView.Object, mockedService.Object, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+        {
+            var mockedView = new Mock<ISearchView>();
+            var mockedService = new Mock<IProductService>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var presenter = new SearchPresenter(mockedView.Object, mockedService.Object, mockedFactory.Object);
+
+            Assert.IsNotNull(presenter);
+        }
+    }
+}
