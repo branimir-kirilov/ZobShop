9aea5a34e754d4e0e0b7abd27f93ed7bc10f5289
Ivan <madritistaaa@gmail.com>
2017-02-18 15:19:53 +0200

    Add add to shopping cart presenter constructor tests

--- src/ZobShop.Web/Product/ProductsList.aspx.cs
+++ src/ZobShop.Web/Product/ProductsList.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using WebFormsMvp;
 using WebFormsMvp.Web;
 using ZobShop.ModelViewPresenter.Product.Details;
