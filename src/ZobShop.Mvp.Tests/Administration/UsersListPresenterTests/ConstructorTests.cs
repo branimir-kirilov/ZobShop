5332d3490ae11b7babdc9de8d93d1f84e780b69d
Ivan <madritistaaa@gmail.com>
2017-02-19 13:17:49 +0200

    UsersListPresenter constructor tests

---
+++ src/ZobShop.Mvp.Tests/Administration/UsersListPresenterTests/ConstructorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using ZobShop.ModelViewPresenter;
+using ZobShop.ModelViewPresenter.Administration.ProductsList;
+using ZobShop.ModelViewPresenter.Administration.UsersList;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Mvp.Tests.Administration.UsersListPresenterTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassServiceNull_ShouldThrowArgumentNullException()
+        {
+            var mockedView = new Mock<IUserListView>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            Assert.Throws<ArgumentNullException>(() =>
+            new UserListPresenter(mockedView.Object, null, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassFactoryNull_ShouldThrowArgumentNullException()
+        {
+            var mockedView = new Mock<IUserListView>();
+            var mockedService = new Mock<IUserService>();
+
+            Assert.Throws<ArgumentNullException>(() =>
+             new UserListPresenter(mockedView.Object, mockedService.Object, null));
+        }
+
+        [Test]
+        public void TestConstructor_ShouldInitializeCorrectly()
+        {
+            var mockedView = new Mock<IUserListView>();
+            var mockedService = new Mock<IUserService>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var presenter = new UserListPresenter(mockedView.Object, mockedService.Object, mockedFactory.Object);
+
+            Assert.IsNotNull(presenter);
+        }
+    }
+}
