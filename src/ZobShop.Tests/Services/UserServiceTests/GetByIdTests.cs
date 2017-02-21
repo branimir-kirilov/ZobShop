53654a9690732a760b2d60c6f97a0dd5f44a457b
Ivan <madritistaaa@gmail.com>
2017-02-21 14:43:22 +0200

    Service get by id tests

---
+++ src/ZobShop.Tests/Services/UserServiceTests/GetByIdTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.Data.Contracts;
+using ZobShop.Models;
+using ZobShop.Services;
+
+namespace ZobShop.Tests.Services.UserServiceTests
+{
+    [TestFixture]
+    public class GetByIdTests
+    {
+        [TestCase("47a46953-1469-438b-a025-a73e3942cbb3")]
+        [TestCase("cbf17e36-013a-4591-8697-5e3ac2dc7fc7")]
+        [TestCase("92813baa-0db4-403f-8c1c-1c7d4dfa7f40")]
+        public void TestGetById_PassValidId_ShouldCallRepositoryGetById(string id)
+        {
+            var repository = new Mock<IRepository<User>>();
+
+            var service = new UserService(repository.Object);
+            service.GetById(id);
+
+            repository.Verify(r => r.GetById(id), Times.Once);
+        }
+
+        [TestCase("47a46953-1469-438b-a025-a73e3942cbb3")]
+        [TestCase("cbf17e36-013a-4591-8697-5e3ac2dc7fc7")]
+        [TestCase("92813baa-0db4-403f-8c1c-1c7d4dfa7f40")]
+        public void TestGetById_PassValidId_ShouldReturnCorrectly(string id)
+        {
+            var mockedUser = new Mock<User>();
+
+            var repository = new Mock<IRepository<User>>();
+            repository.Setup(r => r.GetById(It.IsAny<object>())).Returns(mockedUser.Object);
+
+            var service = new UserService(repository.Object);
+            var result = service.GetById(id);
+
+            Assert.AreSame(mockedUser.Object, result);
+        }
+    }
+}
