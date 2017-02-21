10a5fd1d1b518b8c21fccb697d7079719feaaff6
Ivan <madritistaaa@gmail.com>
2017-02-21 23:51:43 +0200

    Finish model tests

---
+++ src/ZobShop.Tests/Models/ProductOrderTests/ConstructorTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.Models;
+
+namespace ZobShop.Tests.Models.ProductOrderTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase(1)]
+        [TestCase(12)]
+        public void TestConstructor_ShouldInitializeCorrectly(int quantity)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var productOrder = new ProductOrder(mockedProduct.Object, quantity);
+
+            Assert.IsNotNull(productOrder);
+        }
+
+        [TestCase(1)]
+        [TestCase(12)]
+        public void TestConstructor_ShouldSetProductCorrectly(int quantity)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var productOrder = new ProductOrder(mockedProduct.Object, quantity);
+
+            Assert.AreSame(mockedProduct.Object, productOrder.Product);
+        }
+
+        [TestCase(1)]
+        [TestCase(12)]
+        public void TestConstructor_ShouldSetQuantityCorrectly(int quantity)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var productOrder = new ProductOrder(mockedProduct.Object, quantity);
+
+            Assert.AreEqual(quantity, productOrder.Quantity);
+        }
+    }
+}
---
+++ src/ZobShop.Tests/Models/ProductOrderTests/PropertiesTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.Models;
+
+namespace ZobShop.Tests.Models.ProductOrderTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase(1)]
+        [TestCase(12)]
+        public void TestProductOrderId_ShouldWorkCorrectly(int id)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var productOrder = new ProductOrder(mockedProduct.Object, 0);
+            productOrder.ProductOrderId = id;
+
+            Assert.AreEqual(id, productOrder.ProductOrderId);
+        }
+
+        [TestCase(1)]
+        [TestCase(12)]
+        public void TestProductId_ShouldWorkCorrectly(int id)
+        {
+            var mockedProduct = new Mock<Product>();
+
+            var productOrder = new ProductOrder(mockedProduct.Object, 0);
+            productOrder.ProductId = id;
+
+            Assert.AreEqual(id, productOrder.ProductId);
+        }
+    }
+}
