333c4e3f0d5ae137154e9aaba2fa0a43fc530aa9
Branimir <brani_svetoslavov@abv.bg>
2017-02-21 18:41:59 +0200

    Add MyInit tests for checkout presenter

---
+++ src/ZobShop.Mvp.Tests/ShoppingCart/CheckoutPresenterTests/ViewMyInitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using ZobShop.Authentication;
+using ZobShop.Models;
+using ZobShop.ModelViewPresenter;
+using ZobShop.ModelViewPresenter.ShoppingCart.Checkout;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Mvp.Tests.ShoppingCart.CheckoutPresenterTests
+{
+    [TestFixture]
+    public class ViewMyInitTests
+    {
+        [TestCase("Id1")]
+        [TestCase("Id2-12-423-sad")]
+        public void TestViewMyInit_ShouldCallCreateCheckoutViewModel(string id)
+        {
+            var mockedView = new Mock<ICheckoutView>();
+            mockedView.Setup(v => v.Model).Returns(new CheckoutViewModel());
+
+
+            var user = new User() {Id = id };
+      
+            var mockedUserService = new Mock<IUserService>();
+            mockedUserService.Setup(u => u.GetById(It.IsAny<string>())).Returns(user);
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.CurrentUserId).Returns(id);
+
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+            mockedViewModelFactory.Setup(f => f.CreateCheckoutViewModel(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(new CheckoutViewModel());
+
+            var checkoutPresenter = new CheckoutPresenter(mockedView.Object, mockedAuthenticationProvider.Object, mockedUserService.Object, mockedViewModelFactory.Object);
+
+            mockedView.Raise(v => v.MyInit += null, EventArgs.Empty);
+
+            mockedViewModelFactory.Verify(f => f.CreateCheckoutViewModel(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
