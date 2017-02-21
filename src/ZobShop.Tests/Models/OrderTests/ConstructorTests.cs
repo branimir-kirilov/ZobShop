4a8fdf5f91d3f765e589e1c7b2931a97bd214097
Ivan <madritistaaa@gmail.com>
2017-02-21 21:26:52 +0200

    Models tests

---
+++ src/ZobShop.Tests/Models/OrderTests/ConstructorTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using ZobShop.Models;
+
+namespace ZobShop.Tests.Models.OrderTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase("Pesho", "Pesho str", "123456789", 21)]
+        public void TestConstructor_ShouldSetNameCorrectly(string name,
+            string address,
+            string phoneNumber,
+            decimal total)
+        {
+            var order = new Order(name, address, phoneNumber, total);
+
+            Assert.AreEqual(name, order.Name);
+        }
+
+        [TestCase("Pesho", "Pesho str", "123456789", 21)]
+        public void TestConstructor_ShouldSetAddressCorrectly(string name,
+            string address,
+            string phoneNumber,
+            decimal total)
+        {
+            var order = new Order(name, address, phoneNumber, total);
+
+            Assert.AreEqual(address, order.Address);
+        }
+
+        [TestCase("Pesho", "Pesho str", "123456789", 21)]
+        public void TestConstructor_ShouldSetPhoneNumberCorrectly(string name,
+            string address,
+            string phoneNumber,
+            decimal total)
+        {
+            var order = new Order(name, address, phoneNumber, total);
+
+            Assert.AreEqual(phoneNumber, order.PhoneNumber);
+        }
+
+        [TestCase("Pesho", "Pesho str", "123456789", 21)]
+        public void TestConstructor_ShouldSetTotalCorrectly(string name,
+            string address,
+            string phoneNumber,
+            decimal total)
+        {
+            var order = new Order(name, address, phoneNumber, total);
+
+            Assert.AreEqual(total, order.Total);
+        }
+    }
+}
---
+++ src/ZobShop.Tests/Models/OrderTests/PropertiesTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using NUnit.Framework;
+using ZobShop.Models;
+
+namespace ZobShop.Tests.Models.OrderTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void TestIsCompleted_ShouldWorkCorrectly(bool isCompleted)
+        {
+            var order = new Order { IsCompleted = isCompleted };
+
+            Assert.AreEqual(isCompleted, order.IsCompleted);
+        }
+
+        [Test]
+        public void TestProducts_ShouldWorkCorrectly()
+        {
+            var products = new Collection<ProductOrder>();
+
+            var order = new Order { Products = products };
+
+            CollectionAssert.AreEqual(products, order.Products);
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(130)]
+        public void TestOrderId_ShouldWorkCorrectly(int id)
+        {
+            var order = new Order { OrderId = id };
+
+            Assert.AreEqual(id, order.OrderId);
+        }
+    }
+}
