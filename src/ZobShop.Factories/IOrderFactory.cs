bbc84377b2207cd940832f5647993de2134d0410
Ivan <madritistaaa@gmail.com>
2017-02-21 21:00:04 +0200

    Order factory

---
+++ src/ZobShop.Factories/IOrderFactory.cs
@@ -0,0 +1,11 @@
+﻿using ZobShop.Models;
+
+namespace ZobShop.Factories
+{
+    public interface IOrderFactory
+    {
+        Order CreateOrder(string name, string address, string phoneNumber, decimal total);
+
+        ProductOrder CreateProductOrder(Product product, int quantity);
+    }
+}
