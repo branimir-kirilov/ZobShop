62a15616bcf7ed2986a3ada952e8c57f23cef9f9
Branimir <brani_svetoslavov@abv.bg>
2017-02-21 18:01:10 +0200

    Finished rating product tests

--- src/ZobShop.Mvp.Tests/Product/ProductDetailsPresenterTests/OnRatingTests.cs
+++ src/ZobShop.Mvp.Tests/Product/ProductDetailsPresenterTests/OnRatingTests.cs
@@ -42,5 +42,36 @@ namespace ZobShop.Mvp.Tests.Product.ProductDetailsPresenterTests
             mockedService.Verify(s => s.CreateProductRating(rating, content, productId, mockedProvider.Object.CurrentUserId),
                 Times.Once);
         }
+
+        [TestCase(4, "SomeContent", 5, "UserId-1")]
+        [TestCase(-2, "Content", 10, "UserId-2")]
+        public void TestOnRatingProduct_ShouldGetProviderIdCorrectly(
+           int rating,
+           string content,
+           int productId,
+           string userId)
+        {
+            var mockedView = new Mock<IProductDetailsView>();
+            mockedView.Setup(v => v.Model).Returns(new ProductDetailsViewModel());
+
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            mockedProvider.Setup(v => v.CurrentUserId).Returns(userId);
+
+            var mockedService = new Mock<IProductRatingService>();
+            mockedService.Setup(
+                s =>
+                    s.CreateProductRating(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<string>()))
+                        .Returns(new Models.ProductRating());
+
+            var mockedProductService = new Mock<IProductService>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var presenter = new ProductDetailsPresenter(mockedView.Object, mockedProductService.Object, mockedFactory.Object, mockedService.Object, mockedProvider.Object);
+
+            var args = new RateProductEventArgs(rating, content, productId);
+            mockedView.Raise(v => v.RateProduct += null, args);
+
+            mockedProvider.Verify(p => p.CurrentUserId, Times.Once);
+        }
     }
 }
