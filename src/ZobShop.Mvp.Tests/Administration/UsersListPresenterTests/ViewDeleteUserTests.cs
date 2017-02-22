ab76d1ef371fb9063578e414f98f2a768f3a333c
Ivan <madritistaaa@gmail.com>
2017-02-22 09:32:21 +0200

    Add more tests

---
+++ src/ZobShop.Mvp.Tests/Administration/UsersListPresenterTests/ViewDeleteUserTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZobShop.ModelViewPresenter;
+using ZobShop.ModelViewPresenter.Administration.UsersList;
+using ZobShop.Services.Contracts;
+
+namespace ZobShop.Mvp.Tests.Administration.UsersListPresenterTests
+{
+    [TestFixture]
+    public class ViewDeleteUserTests
+    {
+        [TestCase("2e2c6146-caf9-4091-9c8a-f11128dc8f39")]
+        [TestCase("682caaec-71ac-4dd1-82f5-d05e5817f943")]
+        public void TestViewDeleteUser_ShouldCallServiceDeleteUserCorrectly(string userId)
+        {
+            var mockedView = new Mock<IUserListView>();
+            mockedView.Setup(v => v.Model).Returns(new UserListViewModel());
+
+            var mockedService = new Mock<IUserService>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var presenter = new UserListPresenter(mockedView.Object, mockedService.Object, mockedFactory.Object);
+
+            var args = new UserIdEventArgs(userId);
+            mockedView.Raise(v => v.DeleteUser += null, args);
+
+            mockedService.Verify(s => s.DeleteUser(userId), Times.Once);
+        }
+    }
+}
---
+++ src/ZobShop.Mvp.Tests/VIewModels/ProductDetailsViewModelTests/ConstructorTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+using ZobShop.ModelViewPresenter.Product.Details;
+
+namespace ZobShop.Mvp.Tests.VIewModels.ProductDetailsViewModelTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase(1, "TestProduct", "Zob", 10, 5.00, 2.00, "TestMaker", ".jpg")]
+        public void Constructor_Should_SetNameCorrectly(int id,
+            string name,
+           string category,
+           int quantity,
+           decimal price,
+           double volume,
+           string maker,
+           string imageMimeType)
+        {
+            var buffer = new byte[2];
+
+            var product = new ProductDetailsViewModel(id, name, category, price, volume, maker, imageMimeType, buffer);
+
+            Assert.AreEqual(name, product.Name);
+        }
+
+        [TestCase(1, "TestProduct", "Zob", 10, 5.00, 2.00, "TestMaker", ".jpg")]
+        public void Constructor_Should_SetImageBufferCorrectly(int id,
+            string name,
+           string category,
+           int quantity,
+           decimal price,
+           double volume,
+           string maker,
+           string imageMimeType)
+        {
+            var buffer = new byte[2];
+
+            var product = new ProductDetailsViewModel(id, name, category, price, volume, maker, imageMimeType, buffer);
+
+            Assert.AreEqual(buffer, product.ImageBuffer);
+        }
+
+        [TestCase(1, "TestProduct", "Zob", 10, 5.00, 2.00, "TestMaker", ".jpg")]
+        public void Constructor_Should_SetImageMimeTypeCorrectly(int id,
+            string name,
+           string category,
+           int quantity,
+           decimal price,
+           double volume,
+           string maker,
+           string imageMimeType)
+        {
+            var buffer = new byte[2];
+
+            var product = new ProductDetailsViewModel(id, name, category, price, volume, maker, imageMimeType, buffer);
+
+            Assert.AreEqual(imageMimeType, product.ImageMimeType);
+        }
+
+        [TestCase(1, "TestProduct", "Zob", 10, 5.00, 2.00, "TestMaker", ".jpg")]
+        public void Constructor_Should_SetMakerCorrectly(int id,
+            string name,
+           string category,
+           int quantity,
+           decimal price,
+           double volume,
+           string maker,
+           string imageMimeType)
+        {
+            var buffer = new byte[2];
+
+            var product = new ProductDetailsViewModel(id, name, category, price, volume, maker, imageMimeType, buffer);
+
+            Assert.AreEqual(maker, product.Maker);
+        }
+
+        [TestCase(1, "TestProduct", "Zob", 10, 5.00, 2.00, "TestMaker", ".jpg")]
+        public void Constructor_Should_SetPriceCorrectly(int id,
+            string name,
+           string category,
+           int quantity,
+           decimal price,
+           double volume,
+           string maker,
+           string imageMimeType)
+        {
+            var buffer = new byte[2];
+
+            var product = new ProductDetailsViewModel(id, name, category, price, volume, maker, imageMimeType, buffer);
+
+            Assert.AreEqual(price, product.Price);
+        }
+
+        [TestCase(1, "TestProduct", "Zob", 10, 5.00, 2.00, "TestMaker", ".jpg")]
+        public void Constructor_Should_SetVolumeCorrectly(int id,
+            string name,
+           string category,
+           int quantity,
+           decimal price,
+           double volume,
+           string maker,
+           string imageMimeType)
+        {
+            var buffer = new byte[2];
+
+            var product = new ProductDetailsViewModel(id, name, category, price, volume, maker, imageMimeType, buffer);
+
+            Assert.AreEqual(volume, product.Volume);
+        }
+    }
+}
